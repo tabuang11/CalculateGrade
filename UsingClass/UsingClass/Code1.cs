e1aaac7ee1af161a8fc78f3062d3ab592913659a
Tabuang Jandam <JaTabuang@central.co.th>
2020-02-11 09:59:06 +0700

    Update using class for Test all file

---
+++ UsingClass/UsingClass/Code1.cs
@@ -0,0 +1,45 @@
+﻿namespace Code1Data
+{
+    public class Code1
+    {
+        public string dataGrade;
+        public Code1(int score)
+        {
+            string grade;
+            if (score >= 80)
+            {
+                grade = "A";
+            }
+            else if (score >= 75)
+            {
+                grade = "B+";
+            }
+            else if (score >= 70)
+            {
+                grade = "B";
+            }
+            else if (score >= 65)
+            {
+                grade = "C+";
+            }
+            else if (score >= 60)
+            {
+                grade = "C";
+            }
+            else if (score >= 55)
+            {
+                grade = "D+";
+            }
+            else if (score >= 50)
+            {
+                grade = "D";
+            }
+            else
+            {
+                grade = "F";
+            }
+
+            dataGrade = grade;
+        }
+    }
+}
---
+++ UsingClass/UsingClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Code1Data;
+
+namespace UsingClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {    
+            Console.Write("Please input your grade here : ");
+            int dataPoint = Console.Read();
+            Code1 codedata = new Code1(dataPoint);
+            Console.WriteLine(codedata.dataGrade);
+
+        }
+
+    }
+}
